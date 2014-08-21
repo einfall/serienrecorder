40f27a3a9ca304558f9ac43d4750ba932c1588c8
MacDisein <macdisein@gmx.de>
2014-08-21 22:51:55 +0200

    Fixed getImageVersionString for Dreambox
    No more greenscreen in getImageVersionString with Dreambox
    getImageVersionString return result now on Dreambox (Enigmaversion /
    Imageversion) instead of (Creator / Imageversion)

--- src/plugin.py
+++ src/plugin.py
@@ -1568,12 +1568,12 @@ def getImageVersionString():
 	creator = _("n/a")
 	version = _("n/a")
 	isDreambox = False
- 
+
 	try:
 		if fileExists(resolveFilename(SCOPE_SYSETC, 'enigma2/Dream')):
 			# it's a Dreambox
 			isDreambox = True
- 
+
 		file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
 		lines = file.readlines()
 		for x in lines:
@@ -1583,7 +1583,7 @@ def getImageVersionString():
 				creator = creator[0].strip(' ')
 			if splitted[0] == "version":
 				if isDreambox:
- 					if splitted[0] == "version":
+					if splitted[0] == "version":
 						#     YYYY MM DD hh mm
 						#0120 2005 11 29 01 16
 						#0123 4567 89 01 23 45
