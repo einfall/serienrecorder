254ccb6a900ff44bc80707d7c4e4ca874cb1eb8f
w22754 <endlicher@spea-ate.de>
2014-09-03 19:19:31 +0200

    Bugfix: Anzeige bei Marker hinzufügen in Neue Serie(n)/Neue Staffe(n)

--- src/plugin.py
+++ src/plugin.py
@@ -3694,14 +3694,16 @@ class serienRecMarker(Screen):
 		self.session.openWithCallback(self.SetupFinished, serienRecMarkerSetup, serien_name)
 
 	def SetupFinished(self, result):
-		self.readSerienMarker()
+		if result:
+			self.changesMade = True
+			self.readSerienMarker()
 		return
 		
 	def readLogFile(self):
 		self.session.open(serienRecReadLog)
 		
 	def showProposalDB(self):
-		self.session.open(serienRecShowSeasonBegins)
+		self.session.openWithCallback(self.SetupFinished, serienRecShowSeasonBegins)
 
 	def modifyAddedFile(self):
 		self.session.open(serienRecModifyAdded)
@@ -7287,6 +7289,7 @@ class serienRecShowSeasonBegins(Screen):
 		self.yellow = 0xbab329
 		self.white = 0xffffff
 		
+		self.changesMade = False
 		self.proposalList = []
 		self.onLayoutFinish.append(self.readProposal)
 		self.onClose.append(self.__onClose)
@@ -7343,7 +7346,7 @@ class serienRecShowSeasonBegins(Screen):
 			cCursor.execute("SELECT Serie, Staffel, Sender, StaffelStart, UTCStaffelStart, Url, CreationFlag FROM NeuerStaffelbeginn WHERE CreationFlag=? OR CreationFlag>=1 GROUP BY Serie, Staffel", (self.filter, ))
 		for row in cCursor:
 			(Serie, Staffel, Sender, Datum, UTCTime, Url, CreationFlag) = row
-			self.proposalList.append((Serie, str(Staffel).zfill(2), Sender, Datum, UTCTime, Url, CreationFlag))
+			self.proposalList.append((Serie, Staffel, Sender, Datum, UTCTime, Url, CreationFlag))
 		cCursor.close()
 		
 		self['title'].setText(_("Neue Serie(n) / Staffel(n):"))
@@ -7365,7 +7368,7 @@ class serienRecShowSeasonBegins(Screen):
 		else:
 			setFarbe = self.white
 			
-		Staffel = "S%sE01" % Staffel
+		Staffel = "S%sE01" % str(Staffel).zfill(2)
 		WochenTag=[_("Mo"), _("Di"), _("Mi"), _("Do"), _("Fr"), _("Sa"), _("So")]
 		xtime = time.strftime(WochenTag[time.localtime(int(UTCTime)).tm_wday]+", %d.%m.%Y", time.localtime(int(UTCTime)))
 
@@ -7508,6 +7511,7 @@ class serienRecShowSeasonBegins(Screen):
 				cCursor.execute("UPDATE OR IGNORE NeuerStaffelbeginn SET CreationFlag=0 WHERE Serie=? AND Staffel=? AND Sender=? AND StaffelStart=?", data)
 				dbSerRec.commit()
 				cCursor.close()
+				self.changesMade = True
 			self.readProposal()
 		
 	def keyYellow(self):
@@ -7577,7 +7581,7 @@ class serienRecShowSeasonBegins(Screen):
 			self.displayTimer = None
 
 	def keyCancel(self):
-		self.close()
+		self.close(self.changesMade)
 
 	def dataError(self, error):
 		print error
@@ -8418,7 +8422,7 @@ class serienRecMain(Screen):
 		self.session.open(serienRecModifyAdded)
 
 	def showProposalDB(self):
-		self.session.open(serienRecShowSeasonBegins)
+		self.session.openWithCallback(self.readWebpage, serienRecShowSeasonBegins)
 
 	def serieInfo(self):
 		if self.loading:
