65307e22fbe0322b878e99bd46e5db1fe68d1f51
w22754 <endlicher@spea-ate.de>
2014-09-21 16:59:44 +0200

    Bugfix: aufwecken aus DSB

--- src/plugin.py
+++ src/plugin.py
@@ -12,6 +12,7 @@ def SRstart(session, **kwargs):
 
 def Plugins(**kwargs):
 	return [
+		PluginDescriptor(where=[PluginDescriptor.WHERE_SESSIONSTART, PluginDescriptor.WHERE_AUTOSTART], fnc=SerienRecorder.autostart, wakeupfnc=SerienRecorder.getNextWakeup),
 		PluginDescriptor(name="SerienRecorder", description="Record your favorite series.",where = [PluginDescriptor.WHERE_PLUGINMENU], icon = "plugin.png", fnc=SRstart),
 		PluginDescriptor(name="SerienRecorder", description="Record your favorite series.", where = [PluginDescriptor.WHERE_EXTENSIONSMENU], fnc=SRstart)
 		]
